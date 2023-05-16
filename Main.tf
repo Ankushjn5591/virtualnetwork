4349f38fc14888f7a6c8d28b5a94b99801129d9f
Ankush Jain <ankushjn38@gmaial.com>
2023-05-16 20:54:28 +0530

    c

--- Main.tf
+++ Main.tf
@@ -18,7 +18,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
-
 terraform {
   backend "azurerm" {
     resource_group_name  = "Storagerg"
